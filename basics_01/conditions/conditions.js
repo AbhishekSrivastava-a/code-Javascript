ebcbcdb409677627fb700ceb581d28ed8e61b451
Abhishek Srivastava <avix1011.a@gmail.com>
2025-07-01 12:20:10 +0530

    Condition Statements

---
+++ basics_01/conditions/conditions.js
@@ -0,0 +1,20 @@
+// // if - else - else if - switch
+
+var temperature = 41;
+
+if(temperature ===  20){
+    console.log("It is a bit cold today");
+}
+if(temperature < 20){
+    console.log("It is cold today");
+} // no output
+
+if(temperature > 30 && temperature < 40){
+    console.log("The temperature is moderate today")
+}else {
+    console.log("It is very hot")
+}
+
+
+
+
---
+++ basics_01/conditions/conditions_2.js
@@ -0,0 +1,16 @@
+//ternary operators
+
+//to show user if signed in / signed out
+// if signed in show in the page of signout
+//if not signed in then show login
+
+var authenticated = false;
+
+authenticated ? console.log("SignOut") : console.log("LOGIN");
+
+
+//output
+
+
+// SignOut -- when true
+// LOGIN -- when false
